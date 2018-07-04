d32fce5ed73acc4bdeba7623d9c35dde141bb0aa
Clemens Novak <clemens@familie-novak.net>
2018-07-04 16:48:41 +0200

    additional stuff for structs

--- struct_stuff.rkt
+++ struct_stuff.rkt
@@ -4,8 +4,15 @@
 ; by defining a struct, we get a bunch of additional methods:
 
 (style? s)
+
+; getters
 (style-color s)
 (style-size s)
 (style-weight s)
 
 
+; and if the whole thing is set as mutable, we get setters as well
+
+(set-style-color! s "green")
+(set-style-size! s 10)
+(set-style-weight! s 'italic)
