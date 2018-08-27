f12ff36f4beb033d17c59630b3cbcd16c487da89
Clemens Novak <clemens@familie-novak.net>
2018-08-27 22:05:25 +0200

    count word frequency

--- hash_stuff.rkt
+++ hash_stuff.rkt
@@ -13,3 +13,23 @@
 
 
 (hash-ref ht3 1)
+(hash-ref ht3 3 0) ; provide a default value to return if key is not found
+
+
+(define ht4 (hash "one" 1 "two" 2))
+
+(define (inc x) (+ x 1))
+
+(hash-update ht4 "three" inc 0)
+
+
+(define (word-cnt lst)
+	(foldl (lambda (x acc)
+		(hash-update acc x inc 0))
+		(hash)
+		lst))
+
+(word-cnt (string-split "hallo clemens hallo hallo"))
+
+
+
